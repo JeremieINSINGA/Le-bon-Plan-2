7099944715e35061f2fbebade701eadfc2723dff
Semou <semou.ach@gmail.com>
2021-10-25 13:57:13 +0200

    fixe merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
-import "./App.css"
-import "bootstrap/dist/css/bootstrap.min.css"
-
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+import Navbar from "./component/Navbar";
+import Admin from "./component/Admin";
 import Home from "./cities/home";
-import Paris from './cities/paris';
+import Paris from "./cities/paris";
 import Lyon from "./cities/lyon";
 import Marseille from "./cities/marseille";
 import Error404 from "./cities/error404";
@@ -13,8 +15,9 @@ export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
-
+        <Navbar />
         <Switch>
+          <Route exact path="/Admin" component={Admin} />
           <Route exact path="/" component={Home} />
           <Route path="/Paris" component={Paris} />
           <Route path="/Lyon" component={Lyon} />
@@ -22,6 +25,6 @@ export default class App extends Component {
           <Route path="*" component={Error404} />
         </Switch>
       </BrowserRouter>
-    )
+    );
   }
 }
