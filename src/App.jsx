b576c4d3135ea082181870d719bdeecef444960b
Muneeb Rasheed <mr.muneebrasheed@gmail.com>
2021-10-26 10:58:46 +0200

    Navbar Login correction

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,24 @@ import Marseille from "./cities/marseille";
 import Error404 from "./component/error404";
 import Login from "./component/Login";
 
-
 export default class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/Admin" component={Admin} />
-          <Route exact path="/Login" component={Login} />
-          <Route exact path="/" component={Home} />
-          <Route path="/products/cities/Paris" component={Paris} />
-          <Route path="/products/cities/Lyon" component={Lyon} />
-          <Route path="/products/cities/Marseille" component={Marseille} />
-          <Route path="*" component={Error404} />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/Admin" component={Admin} />
+                    <Route exact path="/Login" component={Login} />
+                    <Route exact path="/" component={Home} />
+                    <Route path="/products/cities/Paris" component={Paris} />
+                    <Route path="/products/cities/Lyon" component={Lyon} />
+                    <Route
+                        path="/products/cities/Marseille"
+                        component={Marseille}
+                    />
+                    <Route path="*" component={Error404} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
 }
